0c4767fd5c9bf9d6b12b6181b291fa1a9793f5b2
Xamy9 <samueleneh37@gmail.com>
2026-08-02 21:29:16 +0100

    Configure SMTP email settings for production

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -38,7 +38,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "False"
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 
 
@@ -210,15 +210,27 @@ MESSAGE_TAGS = {
 
 
 
+
+
+# ==========================
+# Email Configuration
+# ==========================
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "False") == "True"
+
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
 ADMIN_EMAIL = os.getenv("ADMIN_EMAIL")
-
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
-
-
 
 
 
